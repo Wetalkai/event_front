72088cc3fb62cdf773b94344d92bce67e4561e71
AlexRayon <arayon@imaginandtonic.com>
2024-03-04 10:42:02 +0100

    update tests

--- tipoPruebas.js
+++ tipoPruebas.js
@@ -5,7 +5,108 @@ window.tests = {
         baseIndex: "index_normal.html",
         url: "https://wetalkai.github.io/event_front?tipoPrueba=zapatosiguales"
     },
-    
+    mismaaficion: {
+        title: "Misma afición",
+        description: "Hazte una foto con alguien que tenga la misma afición que tú (en la foto haced un gesto que lo demuestre)",
+        baseIndex: "index_normal.html",
+        url: "https://wetalkai.github.io/event_front?tipoPrueba=mismaaficion"
+    },
+    /*noconocidolugardeorigen: {
+        title: "Lugar de origen",
+        description: "Hazte una foto con alguien que no conozcas pero que haya nacido en la misma Comunidad Autónoma que tú",
+        baseIndex: "index_normal.html",
+        url: "https://wetalkai.github.io/event_front?tipoPrueba=noconocidolugardeorigen"
+    },*/
+    muchascaras: {
+        title: "Muchas caras!",
+        description: "Hazte una foto con tres personas que no conozcas y que hayan nacido en el mismo mes que tú. Si no lo encuentras, bebes doble!",
+        baseIndex: "index_normal.html",
+        helper:"muchasCaras.js",
+        url: "https://wetalkai.github.io/event_front?tipoPrueba=muchascaras"
+    },
+    emperadoresromanos: {
+        title: "Emperadores romanos!",
+        description: "Haz una foto de dos emperadores romanos, uno ordenando la muerte del gladiador y otro su perdón!",
+        baseIndex: "index_normal.html",
+        helper:"emperadoresRomanos.js",
+        url: "https://wetalkai.github.io/event_front?tipoPrueba=emperadoresromanos"
+    },
+    saludoV: {
+        title: "Saluda como se merece!",
+        description: "Te encuentras por la calle a Spock, cómo le saludas?",
+        baseIndex: "index_normal.html",
+        helper:"saludoV.js",
+        url: "https://wetalkai.github.io/event_front?tipoPrueba=saludoV"
+    },
+    emojis: {
+        title: "Emojis!",
+        description: "😲 😲",
+        baseIndex: "index_normal.html",
+        helper:"emojis.js",
+        url: "https://wetalkai.github.io/event_front?tipoPrueba=emojis"
+    },
+    noParpadees: {
+        title: "Sin parpadeos!",
+        description: "Seréis capaz de juntaros 2 personas sin parpadear durante 20 segundos?",
+        baseIndex: "index_normal.html",
+        helper:"noParpadees.js",
+        url: "https://wetalkai.github.io/event_front?tipoPrueba=noParpadees"
+    },
+    imitaPelicula: {
+        title: "De película!",
+        description: "Con un grupo, elige una escena icónica de una película y recreadla lo mejor que podáis.",
+        baseIndex: "index_normal.html",
+        url: "https://wetalkai.github.io/event_front?tipoPrueba=imitaPelicula"
+    },
+    /*obraArte: {
+        title: "Viva el Arte!",
+        description: "Elige una obra de arte famosa y recreadla con tus amigos. ¡Pon atención a los detalles!",
+        baseIndex: "index_normal.html",
+        url: "https://wetalkai.github.io/event_front?tipoPrueba=obraArte"
+    },*/
+    bebidaGanadora: {
+        title: "Bebida ganadora!",
+        description: "Busca el grupo con más bebidas iguales y hazte una foto con ellos. Si tu bebida coincide con la del grupo, ¡bebes doble!",
+        baseIndex: "index_normal.html",
+        url: "https://wetalkai.github.io/event_front?tipoPrueba=bebidaGanadora"
+    },
+    deporteComun: {
+        title: "Equipo Soñado",
+        description: "Reúne a personas que compartan el mismo deporte o pasatiempo y demostrad vuestra pasión.",
+        baseIndex: "index_normal.html",
+        url: "https://wetalkai.github.io/event_front?tipoPrueba=deporteComun"
+    },
+    desconocidos: {
+        title: "Desconocidos!",
+        description: "Hazte una foto con un desconocido (que no sea del evento!)",
+        baseIndex: "index_normal.html",
+        url: "https://wetalkai.github.io/event_front?tipoPrueba=desconocidos"
+    },
+    barbudos: {
+        title: "Barbudos!",
+        description: "Haz una foto a todos los barbudos de la fiesta!",
+        baseIndex: "index_normal.html",
+        url: "https://wetalkai.github.io/event_front?tipoPrueba=barbudos"
+    },
+    gritaAlto: {
+        title: "Que se te escuche!",
+        description: "3 personas emitiendo una nota mantenida durante 10 segundos! ¡Si lo lográis, bebéis doble! (quien no, triple)",
+        baseIndex: "index_normal.html",
+        helper:"gritaAlto.js",
+        url: "https://wetalkai.github.io/event_front?tipoPrueba=gritaAlto"
+    },
+    quienNoSale: {
+        title: "¿Quién no ha salido?",
+        description: "Fíjate en la pantalla y localiza la gente que no sale en ninguna foto y hazte una foto con ellos! Si te dejas a alguien bebes doble! (No vale preguntar a los demás)",
+        baseIndex: "index_normal.html",
+        url: "https://wetalkai.github.io/event_front?tipoPrueba=quienNoSale"
+    },
+    silvia: {
+        title: "Ahora todos!",
+        description: "Foto grupal con la anfitriona!!",
+        baseIndex: "index_normal.html",
+        url: "https://wetalkai.github.io/event_front?tipoPrueba=silvia"
+    },
     generica: {
         title: "Deja tu mensaje!",
         description: "",
